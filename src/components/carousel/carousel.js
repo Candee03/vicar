71f0542b88aaca16d6da7bf399a0eada7ce8e0ac
Candela Alfano <candela.alfano1503@gmail.com>
2024-07-03 20:17:04 -0300

    carousel home

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,44 +1,68 @@
 import React, { useEffect, useState } from 'react'
 import './carousel.scss'
+import Btn from '../botton/Btn';
 
-const Carousel = (props) => {
+/**
+ * 
+ * @param {} props imgSize number
+ * @param {} props timeOut number
+ * @param {} props data array
+ * @param {} props arrows boolean
+ * @returns 
+ */
+
+const Carousel = ({data, timeOut, arrows}) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
     const interval = setInterval(() => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % (props.data).length);
-    }, parseInt(props.timeOut));
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % (data).length);
+    }, parseInt(timeOut));
 
     return () => clearInterval(interval);
-    }, [(props.data).length]);
+    }, [(data).length]);
     
     // FUNCION PARA CAMBIAR LA IMAGEN
     const changeImg = (change) => {
         if (change === 'prev') {
-            currentIndex===0? setCurrentIndex((props.data.length-1)): setCurrentIndex(currentIndex-1)
+            currentIndex===0? setCurrentIndex((data.length-1)): setCurrentIndex(currentIndex-1)
         }
         if (change === 'next') {
-            currentIndex===props.data.length-1? setCurrentIndex(0): setCurrentIndex(currentIndex+1)
+            currentIndex===data.length-1? setCurrentIndex(0): setCurrentIndex(currentIndex+1)
         }
     }
     return (
-        <div className='main-carousel'>
+        <div className='main-carousel-home'>
+            {arrows==='true'? 
             <div className='leftArrow' onClick={() =>{changeImg('prev')}}>&#10092;</div>
-            <div className="carousel" style={{width: `${props.imgSize}px`}}>
-                <div className="carousel-images" style={{ transform: `translateX(${-currentIndex * 100}% ` }}
-                >
-                {(props.data).map((item, index) => (
-                    <img 
-                    key={index} 
-                    src={item.imgData} 
-                    alt={`Slide ${index}`} 
-                    width= {props.imgSize} />
+            : <></> }
+            
+            <div className="container-carousel-home">
+                <div className="carousel-home">
+                {(data).map((item, index) => (
+                    <>
+                        <img 
+                        key={index} 
+                        src={item.imgData} 
+                        alt={`Slide ${index}`}  
+                        className={index === currentIndex ? 'active img-carousel-home' : 'disable'}/>
+
+                        <div className={index === currentIndex ? 'active container-info-banner' : 'disable'}>
+                            {item.title? <p className='title-carousel-home'>
+                                {item.title}</p>: <></> }
+                            {item.text? <p className='text-carousel-home'>
+                                {item.text}</p>: <></> }
+                            {item.btnName? <Btn btnType={'home'} innerText={item.btnName}/> : <></>}
+                        </div>
+                    </>
                 ))}
                 </div>
             </div>
+            {arrows==='true'? 
             <div className='rightArrow' onClick={() =>{changeImg('next')}}>&#10093;</div>
+            : <></> }
         </div>
     );
 };
 
-export default Carousel
+export default Carousel;
