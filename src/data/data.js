eb4c8ad63ea55b22ed2f7552f383e40a4256e64a
Candela Alfano <candela.alfano1503@gmail.com>
2024-07-03 20:19:17 -0300

    add images of banner and clientes

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,4 @@
+// IMAGENES dataNuestrosServicios
 import iconosServ from '../img/nuestros-servicios/iconos-serv.png'
 import iconosServ1 from '../img/nuestros-servicios/iconos-serv1.png'
 import iconosServ2 from '../img/nuestros-servicios/iconos-serv2.png'
@@ -5,61 +6,67 @@ import iconosServ3 from '../img/nuestros-servicios/iconos-serv3.png'
 import iconosServ4 from '../img/nuestros-servicios/iconos-serv4.png'
 import iconosServ5 from '../img/nuestros-servicios/iconos-serv5.png'
 
-export const data = [
-    {
-        id:1,
-        imgData:'https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f60e.png',
-        title:'titulo de prueba',
-        text: 'texto de prueba'
-    },
-    {
-        id:2,
-        imgData:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png',
-        title:'titulo de prueba2',
-        text: 'texto de prueba'
-    },
-    {
-        id:3,
-        imgData:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png',
-        title:'titulo de prueba3',
-        text: 'texto de prueba'
-    }
-]
+//IMAGENES NUESTROS CLIENTES
+import cliente1 from '../img/nuestros-clientes/cliente1.png'
+import cliente2 from '../img/nuestros-clientes/cliente2.png'
+import cliente3 from '../img/nuestros-clientes/cliente3.png'
+import cliente4 from '../img/nuestros-clientes/cliente4.png'
+import cliente5 from '../img/nuestros-clientes/cliente5.png'
+import cliente6 from '../img/nuestros-clientes/cliente6.png'
+import cliente7 from '../img/nuestros-clientes/cliente7.png'
+import cliente8 from '../img/nuestros-clientes/cliente8.png'
+import cliente9 from '../img/nuestros-clientes/cliente9.png'
+import cliente10 from '../img/nuestros-clientes/cliente10.png'
+import cliente11 from '../img/nuestros-clientes/cliente11.png'
+import cliente12 from '../img/nuestros-clientes/cliente12.png'
+import cliente13 from '../img/nuestros-clientes/cliente13.png'
+import cliente14 from '../img/nuestros-clientes/cliente14.png'
+import cliente15 from '../img/nuestros-clientes/cliente15.png'
+import cliente16 from '../img/nuestros-clientes/cliente16.png'
+
+// IMAGENES homeBanner
+import img1 from '../img/home-carousel/img1.png'
+import img2 from '../img/home-carousel/img2.png'
+
+
 export const dataNuestrosServicios = [
     {
-        id:1,
         imgData:iconosServ,
-        title:'ALARMAS PARA HOGAR O COMERCIO',
+        title:`ALARMAS 
+        PARA HOGAR O 
+        COMERCIO`,
         text: 'Instalación de equipos de seguridad diseñados a medida.'
     },
     {
-        id:2,
         imgData: iconosServ1,
-        title:'CÁMARAS DE VIGILANCIA',
+        title:`CÁMARAS DE 
+        VIGILANCIA`,
         text: 'Instalación de cámaras de vigilancia y acceso web para que puedas visualizarlas en tiempo real y desde cualquier dispositivo.'
     },
     {
-        id:3,
         imgData: iconosServ2,
-        title:'RESPUESTA MÓVIL',
+        title:`RESPUESTA 
+        MÓVIL`,
         text: 'Amplia flota de vehículos para una respuesta rápida y eficaz.'
     },
     {
-        id:3,
         imgData: iconosServ3,
-        title:'MONITOREO ACTIVO 24/7',
+        title:`MONITOREO 
+        ACTIVO 
+        24/7`,
         text: 'Central de monitoreo funcionando hace más de 30 años.'
     },
     {
-        id:3,
         imgData: iconosServ4,
-        title:'SERVICIO TÉCNICO LAS 24HS',
+        title:`SERVICIO 
+        TÉCNICO 
+        LAS 24HS`,
         text: 'Contamos con personal ampliamente capacitado para solucionar cualquier inconveniente de manera rápida.'
     },
     {
-        id:3,
         imgData: iconosServ5,
-        title:'RASTREO SATELITAL',
+        title:`RASTREO 
+        SATELITAL`,
         text: 'Instalación de GPS y control de flotas.'
     },
 ]
@@ -70,4 +77,41 @@ export const images = [
     'https://st2.depositphotos.com/1000686/8892/i/450/depositphotos_88920606-stock-photo-three-white-labrador-puppy-in.jpg',
     'https://st2.depositphotos.com/1000686/8892/i/450/depositphotos_88920606-stock-photo-three-white-labrador-puppy-in.jpg',
     'https://st2.depositphotos.com/1000686/8892/i/450/depositphotos_88920606-stock-photo-three-white-labrador-puppy-in.jpg',
+]
+export const homeBanner = [
+    {
+        imgData:img1,
+        title:`INNOVAMOS
+        EN SEGURIDAD`,
+        text: 'Tecnología de punta en videoverificación',
+        btnName: 'Más información',
+        btnLink: ''
+    },
+    {
+        imgData:img2,
+        title:`SEGURIDAD
+        A MEDIDA
+        PARA HOGARES
+        O COMERCIOS`,
+        btnName: 'Cotizar',
+        btnLink: ''
+    },
+]
+export const nuestrosClientesData = [
+    cliente1,
+    cliente2,
+    cliente3,
+    cliente4,
+    cliente5,
+    cliente6,
+    cliente7,
+    cliente8,
+    cliente9,
+    cliente10,
+    cliente11,
+    cliente12,
+    cliente13,
+    cliente14,
+    cliente15,
+    cliente16
 ]
