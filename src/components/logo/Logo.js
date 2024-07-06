e8f6df6b47c724ecd949c6712496201f6f160e02
Candela Alfano <candela.alfano1503@gmail.com>
2024-07-06 16:36:37 -0300

    changes in footer and add images. change 100vw for 100%

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import logo from './logo.png'
+import logo2 from './logo-2.png'
 import './Logo.scss'
 import { Link } from 'react-scroll'
 
-const Logo = ({size}) => {
+const Logo = ({size, type}) => {
     return (
         <Link to='home' delay={200} smooth={true} duration={800}>
-            <img width={size} alt='logo de vicar' className='logo-header' src={logo}/>
+            <img width={size} alt='logo de vicar' className='logo-header' src={type==='footer'?logo2: logo}/>
         </Link>
     )
 }
--- src/sections/footer/Footer.js
+++ src/sections/footer/Footer.js
@@ -1,37 +1,62 @@
 import React from 'react'
 import './Footer.scss'
 import Logo from '../../components/logo/Logo'
+//provider
+import instagram from '../../img/footer/instagram.png'
+import email from '../../img/footer/email.png'
+import phone from '../../img/footer/phone.png'
+import whatsapp from '../../img/footer/whatsapp.png'
+import map from '../../img/footer/map.png'
+import { Link } from 'react-scroll'
+
 
 const Footer = () => {
     return (
         <div className='footer'>
             <div className='container-logo-footer'>
-                <Logo size={100}/>
+                <Logo type={'footer'} size={100}/>
             </div>
 
             <div className='container-contact-footer'>
                 <div className='social-media-logos'>
-
+                    <Link href=''>
+                        <img className='img-social-media' alt='' src={whatsapp}/>
+                    </Link>
+                    <Link href=''>
+                        <img className='img-social-media' alt='' src={instagram}/>
+                    </Link>
+                    <Link href=''>
+                        <img className='img-social-media' alt='' src={phone}/>
+                    </Link>
+                    <Link href=''>
+                        <img className='img-social-media' alt='' src={email}/>
+                    </Link>
+                    <Link href=''>
+                        <img className='img-social-media' alt='' src={map}/>
+                    </Link>
                 </div>
                 <div className='contact-data'>
-                    <p>Atención al cliente:</p>
-                    <p>Teléfono: +59424125469</p>
-                    <p>E-mail: vicar@vicar.com</p>
+                    <p className='p-bold-contact-data'>Atención al cliente:</p>
+                    <p className='p-contact-data'>Teléfono: +59424125469</p>
+                    <p className='p-contact-data'>E-mail: vicar@vicar.com</p>
                 </div>
                 <div className='contact-data'>
-                    <p>Cotizaciones:</p>
-                    <p>Teléfono: +56846351351</p>
-                    <p>E-mail: info@vicar.com.uy</p>
+                    <p className='p-bold-contact-data'>Cotizaciones:</p>
+                    <p className='p-contact-data'>Teléfono: +56846351351</p>
+                    <p className='p-contact-data'>E-mail: info@vicar.com.uy</p>
                 </div>
                 <div className='contact-data'>
-                    <p>Dirección:</p>
-                    <p>Av. Gobernador Gómez 2320</p>
-                    <p>San Carlos - Uruguay</p>
+                    <p className='p-bold-contact-data'>Dirección:</p>
+                    <p className='p-contact-data'>Av. Gobernador Gómez 2320</p>
+                    <p className='p-contact-data'>San Carlos - Uruguay</p>
+                </div>
+                <div className='contact-data'>
+                    <p className='p-politica-de-privacidad'>Política de privacidad</p>
                 </div>
             </div>
 
             <div className='container-credits-footer'>
-                <p className='p-credits'>Desarrollado por <a className='a-credits' href='www.bedigital.uno'>BE DIGITAL</a></p>
+                <p className='p-credits'>Desarrollado por <a className='a-credits' href='https://bedigital.uno/'>BE DIGITAL</a></p>
             </div>
         </div>
     )
