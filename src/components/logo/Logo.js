74e74b780907d07212b4e3f8e272c7bdf2eafe3b
Candela Alfano <candela.alfano1503@gmail.com>
2024-07-29 15:53:11 -0300

    delete logo.scss

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,13 +1,12 @@
 import React from 'react'
+import { Link } from 'react-scroll'
 import logo from './logo.png'
 import logo2 from './logo-2.png'
-import './Logo.scss'
-import { Link } from 'react-scroll'
 
 const Logo = ({size, type}) => {
     return (
-        <Link to='home' delay={200} smooth={true} duration={800}>
-            <img width={size} alt='logo de vicar' className='logo-header' src={type==='footer'?logo2: logo}/>
+        <Link to='home' delay={200} smooth={true} duration={800} offset={-60}>
+            <img  width={size} alt='logo de vicar' className='logo-header' src={type==='2'?logo2: logo}/>
         </Link>
     )
 }
