9fbfd91401f2c53fc0cb5172f9e3fa8a9d2366e2
Candela Alfano <candela.alfano1503@gmail.com>
2024-07-29 15:58:11 -0300

    add responsive

--- src/sections/navBar/NavBar.js
+++ src/sections/navBar/NavBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-scroll';
+import { useMediaQuery } from 'react-responsive';
 //styles
 import './NavBar.scss'
 //components
@@ -7,6 +8,7 @@ import Logo from '../../components/logo/Logo';
 import menuLogo from './menu-logo.png'
 
 const Navbar = ({navList}) => {
+    const isDesktop = useMediaQuery({ query: '(min-width: 1000px)' });
     const [deployed, setDeployed] = useState(false)
 
     //funcion para desplegar el nav bar
@@ -28,29 +30,50 @@ const Navbar = ({navList}) => {
     
     return (
         <header>
-            <nav className='nav'>
-                <Logo size={56}/>
-                <button onClick={() => clickNav()} className='btn-nav'><img className='img-menu' alt='' src={menuLogo} /></button>
-            </nav>
-
-            <ul className={deployed?'ul-nav-bar': 'disable'}>
-                {
-                deployed?
-                    <>
-                        {navList.map((li) => {
-                            return (
-                                <li className='li-nav-bar'>
-                                    <Link onClick={() => clickNav()} to={`${parse(li)}`} delay={200} smooth={true} duration={800} offset={-60}>
-                                        {li} 
-                                    </Link>
-                                </li>
-                            )
-                        })}
-                    </>
+            {isDesktop?
+                <nav className='nav'>
+                    <Logo type={'2'} size={92}/>
+                </nav>
                     :
-                    <></>
-                }
-            </ul>
+                <nav className='nav'>
+                    <Logo size={56}/>
+                    <button onClick={() => clickNav()} className='btn-nav'><img className='img-menu' alt='' src={menuLogo} /></button>
+                </nav>
+            }
+
+            {isDesktop?
+                <ul className='ul-nav-bar-desktop'>
+                    {navList.map((li) => {
+                        return (
+                            <li className='li-nav-bar-desktop'>
+                                <Link onClick={() => clickNav()} to={`${parse(li)}`} delay={200} smooth={true} duration={800} offset={-60}>
+                                    {li} 
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+                :
+                <ul className={deployed?'ul-nav-bar': 'disable'}>
+                    {
+                    deployed?
+                        <>
+                            {navList.map((li) => {
+                                return (
+                                    <li className='li-nav-bar'>
+                                        <Link onClick={() => clickNav()} to={`${parse(li)}`} delay={200} smooth={true} duration={800} offset={-60}>
+                                            {li} 
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </>
+                        :
+                        <></>
+                    }
+                </ul>
+                
+            }
             <div onClick={()=> clickNav()} className={deployed?'nav-bar-bg': 'disable'}></div>
         </header>
     );
